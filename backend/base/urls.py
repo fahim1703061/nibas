a914c07a3f6d32d2aa7a814b504ba98b0399caaf
Md. Fahim Faisal <fahimfaisal7373@gmail.com>
2022-05-29 03:25:11 +0600

    django REST api installed working on api

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,7 +1,10 @@
+from unicodedata import name
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('',)
+    path('', views.getRoutes, name='routes'),
+    path('rent/', views.getRentHomes, name='rentHomes'),
+    path('rent/<str:pk>', views.getRentHome, name='rentHome'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,36 @@
+from operator import imod
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .RentHomes_data import rentHomes
+
 
 # Create your views here.
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        'api/rent',
+        'api/rent/create',
+        'api/rent/upload',
+
+        'api/rent/<id>',
+    ]
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getRentHomes(requst):
+    return Response(rentHomes)
+
+
+@api_view(['GET'])
+def getRentHome(requst, pk):
+    rentHome = None
+    for i in rentHomes:
+        if i['_id'] == pk:
+            rentHome = i
+            break
+    return Response(rentHomes)
