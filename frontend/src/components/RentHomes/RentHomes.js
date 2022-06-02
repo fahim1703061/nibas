944ec7ae66483ef4c01a45274d5bd112834ee61d
Md. Fahim Faisal <fahimfaisal7373@gmail.com>
2022-06-02 22:49:47 +0600

    id in url fixed use back tick use back tick for template Template literals

--- frontend/src/components/RentHomes/RentHomes.js
+++ frontend/src/components/RentHomes/RentHomes.js
@@ -9,14 +9,17 @@ function RentHomes( {rentHome}) {
   return (
     <div>
       <Card className='my-3 p-3 rounded' >
-        <Link to={'/rent/:{rentHome._id}'}>
+
+      {/* use back tick for template Template literals
+      ref : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals */}
+        <Link to={`/rent/${rentHome._id}`}>   
             <Card.Img src={rentHome.image} />
         </Link>
 
         <Card.Body>
-            <Link to='/rent/'>
+            <Link to={`/rent/${rentHome._id}`}>
                 <Card.Title as="div" className=''>
-                    <strong style={{color: "black"}}>{rentHome.name}</strong>
+                    <strong style={{color: "black"}}>{rentHome.name} {rentHome._id}</strong>
                 </Card.Title>
             </Link>
             <Card.Text as="div">
